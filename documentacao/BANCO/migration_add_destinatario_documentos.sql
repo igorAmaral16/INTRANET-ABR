7b9ff13e8e223bef682c50a13768c99de5c54b55
Igor Amaral <igoramaraal089@gmail.com>
2026-02-24 14:51:52 -0300

    refator: remoção e ajuste na exibição de arquivos

--- documentacao/BANCO/migration_add_destinatario_documentos.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Add destinatario to BibliotecaDocumentos
--- Date: 2026-02-24
--- Purpose: allow documents to be targeted at a specific collaborator (matrícula)
-
-ALTER TABLE BibliotecaDocumentos
-    ADD COLUMN destinatario_matricula VARCHAR(50) NULL AFTER mime_type;
-
--- index for quick lookup by destinatário
-CREATE INDEX idx_bib_doc_destinatario ON BibliotecaDocumentos(destinatario_matricula);
